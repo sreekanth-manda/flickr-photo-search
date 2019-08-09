b679903c61383d17f4fa1d35840c34421a1cac18
Sreekanth Manda <sreekanth.manda@gmail.com>
2019-08-09 17:04:10 +1000

    Updates

---
+++ precache-manifest.c3647c27444270e002fda2c5e8b6f053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2b049961d6e872512c09b6a426f476",
+    "url": "/flickr-photo-search/index.html"
+  },
+  {
+    "revision": "4efdc38765113e44324b",
+    "url": "/flickr-photo-search/static/css/main.449e9822.chunk.css"
+  },
+  {
+    "revision": "2bf6fa96858afbb0cb71",
+    "url": "/flickr-photo-search/static/js/2.ad525d50.chunk.js"
+  },
+  {
+    "revision": "4efdc38765113e44324b",
+    "url": "/flickr-photo-search/static/js/main.8f9cad03.chunk.js"
+  },
+  {
+    "revision": "9cc7e7bc37cf5c17e513",
+    "url": "/flickr-photo-search/static/js/runtime~main.f3b1f7d1.js"
+  }
+]);
